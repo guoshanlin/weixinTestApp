ded8b00cb1f676a1dd0ffdc72713a410fff31efc
guosl <guosl@coreware.com.cn>
2018-03-19 10:20:59 +0800

    1\微信小程序练习   table列表的实现；   下拉到底部时自动加载数据；   select选中跳转

---
+++ pages/scrollableTabView/index.js
@@ -0,0 +1,119 @@
+// pages/scrollableTabView/index.js
+Page({
+  data: {
+    tabs: ['收入', '支出', '通知', '我的'],
+    scrollJson:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],
+    stv: {
+      windowWidth: 0,
+      lineWidth: 0,
+      offset: 0,
+      tStart: false
+    },
+    activeTab: 0
+  },
+  onLoad: function (options) {
+    try {
+      let { tabs } = this.data;
+      var res = wx.getSystemInfoSync()
+      this.windowWidth = res.windowWidth;
+      this.data.stv.lineWidth = this.windowWidth / this.data.tabs.length;
+      this.data.stv.windowWidth = res.windowWidth;
+      this.setData({ stv: this.data.stv })
+      this.tabsCount = tabs.length;
+    } catch (e) {
+    }
+  },
+  handlerStart(e) {
+    let { clientX, clientY } = e.touches[0];
+    this.startX = clientX;
+    this.tapStartX = clientX;
+    this.tapStartY = clientY;
+    this.data.stv.tStart = true;
+    this.tapStartTime = e.timeStamp;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerMove(e) {
+    let { clientX, clientY } = e.touches[0];
+    let { stv } = this.data;
+    let offsetX = this.startX - clientX;
+    this.startX = clientX;
+    stv.offset += offsetX;
+    if (stv.offset <= 0) {
+      stv.offset = 0;
+    } else if (stv.offset >= stv.windowWidth * (this.tabsCount - 1)) {
+      stv.offset = stv.windowWidth * (this.tabsCount - 1);
+    }
+    this.setData({ stv: stv });
+  },
+  handlerCancel(e) {
+
+  },
+  handlerEnd(e) {
+    let { clientX, clientY } = e.changedTouches[0];
+    let endTime = e.timeStamp;
+    let { tabs, stv, activeTab } = this.data;
+    let { offset, windowWidth } = stv;
+    //快速滑动
+    if (endTime - this.tapStartTime <= 300) {
+      //向左
+      if (Math.abs(this.tapStartY - clientY) < 50) {
+        if (this.tapStartX - clientX > 5) {
+          if (activeTab < this.tabsCount - 1) {
+            this.setData({ activeTab: ++activeTab })
+          }
+        } else {
+          if (activeTab > 0) {
+            this.setData({ activeTab: --activeTab })
+          }
+        }
+        stv.offset = stv.windowWidth * activeTab;
+      } else {
+        //快速滑动 但是Y距离大于50 所以用户是左右滚动
+        let page = Math.round(offset / windowWidth);
+        if (activeTab != page) {
+          this.setData({ activeTab: page })
+        }
+        stv.offset = stv.windowWidth * page;
+      }
+    } else {
+      let page = Math.round(offset / windowWidth);
+      if (activeTab != page) {
+        this.setData({ activeTab: page })
+      }
+      stv.offset = stv.windowWidth * page;
+    }
+    stv.tStart = false;
+    this.setData({ stv: this.data.stv })
+  },
+  _updateSelectedPage(page) {
+    let { tabs, stv, activeTab } = this.data;
+    activeTab = page;
+    this.setData({ activeTab: activeTab })
+    stv.offset = stv.windowWidth * activeTab;
+    this.setData({ stv: this.data.stv })
+  },
+  handlerTabTap(e) {
+    this._updateSelectedPage(e.currentTarget.dataset.index);
+  },
+  refesh:function(e) {
+    console.log("下拉到顶了，重置数据。。。。")
+    this.setData({
+      scrollJson: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+    })
+  },
+  loadMore:function(e){
+    console.log("下拉到底了，加载数据。。。。")
+    var _arr = this.data.scrollJson 
+    var minIndex = _arr[_arr.length-1] +1
+    if(minIndex >80){
+      return
+    }
+    var maxIndex = minIndex + 20
+    for (minIndex; minIndex < maxIndex; minIndex++){
+      _arr.push(minIndex)
+    }
+    this.setData({
+      scrollJson: _arr
+    })
+  }
+})
